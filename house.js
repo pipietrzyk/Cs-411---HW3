911bbb1ab80844d8a3c6ce1369916ecbf72ed0c9
pipietrzyk <pipietrzyk0@gmail.com>
2023-10-31 20:03:14 -0500

    "CS 411 Assignment"

---
+++ house.js
@@ -0,0 +1,64 @@
+var gl;
+var points;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    
+    // Four Vertices
+    
+    var vertices = [
+        vec2( -0.5, -0.5 ),
+        vec2( -0.5,  0.25 ),
+        vec2( 0.5, 0.25 ),
+        vec2( 0.5, -0.5)
+    ];
+
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Uses the buttons to determine the color to use
+    // Default = red
+    var colorToUse = vec4(1.0, 0, 0, 1);
+    document.getElementById("Red").onclick = function() {colorToUse = vec4(1.0, 0, 0, 1);}
+    document.getElementById("Blue").onclick = function() {colorToUse = vec4(0.0, 0, 1.0, 1);}
+    document.getElementById("Yellow").onclick = function() {colorToUse = vec4(1.0, 1.0, 0.8, 1);}
+    document.getElementById("Cyan").onclick = function() {colorToUse = vec4(0.0, 1.0, 1.0, 1);}
+
+
+    canvas.addEventListener("mousedown", function(event){});
+
+
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+}
