21dc6a6ad33ebd0c92da6b4ccd25a4bba1e4f2fd
pipietrzyk <pipietrzyk0@gmail.com>
2023-11-03 11:29:35 -0500

    Fully works

--- house.js
+++ house.js
@@ -5,6 +5,9 @@ var RED = vec4(1.0, 0, 0, 1.0);
 var BLUE = vec4(0, 0, 1.0, 1.0);
 var YELLOW = vec4(1.0, 0.91, 0.0, 1.0);
 var CYAN = vec4(0.0, 1.0, 1.0, 1.0);
+var GREEN = vec4(0, 1, 0, 1);
+var BLACK = vec4(0,0,0,1);
+var PINK = vec4(1, 0.75, 0.79, 1);
 
 window.onload = function init()
 {
@@ -48,40 +51,53 @@ window.onload = function init()
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    
-    //  Load shaders and initialize attribute buffers
-    
+
+    // Each object has its own program
     var program = new Array(3);
 
-    
+    var color = new Array(3);
+    color[0] = BLUE; // Door
+    color[1] = YELLOW // Walls
+    color[2] = RED; // Roof
+
     // Uses the buttons to determine the color to use
-    // Default color = red
-    var colorToUse = vec4(1.0, 0, 0, 0.0, 1.0);
+    var colorToUse = vec4(1,0,0,1);
     document.getElementById("Red").onclick = function() {colorToUse = RED;}
     document.getElementById("Blue").onclick = function() {colorToUse = BLUE;}
     document.getElementById("Yellow").onclick = function() {colorToUse = YELLOW;}
     document.getElementById("Cyan").onclick = function() {colorToUse = CYAN;}
+    document.getElementById("Green").onclick = function() {colorToUse = GREEN;}
+    document.getElementById("Black").onclick = function() {colorToUse = BLACK;}
+    document.getElementById("Pink").onclick = function() {colorToUse = PINK;}
 
+    drawScreen(program, color, vertices);
 
     canvas.addEventListener("mousedown", function(event){
-        //const rect = canvas.getBoundingClientRect();
-        //const x = (event.clientX - rect.left) / canvas.width * 2 - 1;
-        //const y = (event.clientY - rect.top) / canvas.height * 2;
         var x = (event.clientX / 256) - 1;
         var y = ((canvas.height - event.clientY) / 256) - 1;
 
-        if (x >= -0.5 && x <= 0.5 && y >= 0.25) {
-            console.log("ROOF");
-            console.log(x,y);
-        } else if (x > -0.2 && x < 0.2 && y > -0.5 && y < 0.05) {
-            console.log("DOOR");
-            console.log(x,y);
-        } else {
-            console.log("WALL");
-            console.log(x,y);
+        if (x >= -0.5 && x <= 0.5 && y >= 0.25) { // ROOF
+            color[2] = colorToUse;
+            drawScreen(program, color, vertices);
+        } else if (x > -0.2 && x < 0.2 && y > -0.5 && y < 0.05) { // DOOR
+            color[0] = colorToUse;
+            drawScreen(program, color, vertices);
+        } else { // WALLS
+            color[1] = colorToUse;
+            drawScreen(program, color, vertices);
         }
     });
 
+};
+
+
+function render(vertices, fColor, color) {
+
+    gl.uniform4fv(fColor, color);
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, vertices );
+}
+
+function drawScreen(program, color, vertices) {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
     for (let i = 0; i < program.length; i++) {
         program[i] = initShaders( gl, "vertex-shader", "fragment-shader" );
@@ -94,7 +110,7 @@ window.onload = function init()
         gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
         gl.enableVertexAttribArray( vPosition );
 
-        switch (i) {
+        /*switch (i) {
             case 0:
                 colorToUse = BLUE;
                 break;
@@ -103,32 +119,16 @@ window.onload = function init()
                 break;
             case 2:
                 colorToUse = RED;
-        }
+        }*/
 
-        /*var cbuff = gl.createBuffer();
-        gl.bindBuffer( gl.ARRAY_BUFFER, cbuff );
-        gl.bufferData( gl.ARRAY_BUFFER, flatten(colorToUse), gl.STATIC_DRAW );*/
 
-        var fColor = gl.getUniformLocation(program[i], "fColor"); // Gets the location of the variable in the vertex shader
-        console.log(fColor)
-        
-        //gl.vertexAttribPointer(fColor, 3, gl.FLOAT, false, 0, 0);
-        //gl.enableVertexAttribArray(fColor);
-
+        var fColor = gl.getUniformLocation(program[i], "fColor");
 
         var numVertices = 4;
         if (i == 2) {
             numVertices = 3;
         }
 
-        render(numVertices, fColor, colorToUse);
+        render(numVertices, fColor, color[i]);
     }
-
-};
-
-
-function render(vertices, fColor, colorToUse) {
-
-    gl.uniform4fv(fColor, colorToUse);
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, vertices );
 }
